e353108d387f64f76d4ed105e84ce669a15c416d
Zytronium <dastelljes@outlook.com>
2026-02-17 19:20:53 -0600

    Extract Header component from Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,4 @@
-function Header() {
-  return (
-    <div className="flex flex-col items-center">
-      <h1 className="text-6xl font-bold text-text-neon">Free RAM Downloader</h1>
-      <p className="text-xl">Generate and download custom free RAM for your PC!</p>
-      <div className="h-6"></div>
-      <p className="text-slate-600 dark:text-slate-400">
-        Instantly boost your computer&apos;s performance by downloading
-        additional RAM directly from our secure servers, 100% free.
-      </p>
-    </div>
-  )
-}
+import Header from "@/components/Header";
 
 export default function Home() {
   return (
---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <div className="flex flex-col items-center">
+      <h1 className="text-6xl font-bold text-text-neon">Free RAM Downloader</h1>
+      <p className="text-xl">Generate and download custom free RAM for your PC!</p>
+      <div className="h-6"></div>
+      <p className="text-slate-600 dark:text-slate-400">
+        Instantly boost your computer&apos;s performance by downloading
+        additional RAM directly from our secure servers, 100% free.
+      </p>
+    </div>
+  )
+}
